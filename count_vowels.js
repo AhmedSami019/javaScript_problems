ed5536ea509e08faaa7b3c1d35937e772f7bbc1f
AhmedSami019 <easami019192@gmail.com>
2025-10-07 13:38:29 +0600

    count is solve using method 2

--- count_vowels.js
+++ count_vowels.js
@@ -4,9 +4,27 @@ const text = "A quick brown fox jumps over the lazy dog"
     const sentence = text.toLowerCase()
 
     for (let i = 0; i < sentence.length; i++) {
-        console.log(element);
+        const element = sentence[i]
         if(element === 'a' || element === 'e' || element === 'i' || element === 'o' || element === 'u'){
             vowels++
         }
     }
     console.log("the count of vowels are ", vowels);
+
+
+
+    // method 2
+
+    let vowelsCount = 0;
+    const reference = ["a", "e", "i", "o", "u"]
+
+    const strings = text.toLowerCase()
+
+    for (let i = 0; i <= strings.length; i++) {
+        const letter = strings[i];
+        if (reference.includes(letter)) {
+            vowelsCount++
+        }
+ 
+    }
+    console.log(vowelsCount);
