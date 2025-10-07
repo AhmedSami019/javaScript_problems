e854771a0557fef380bdae0d26527279235b53a5
AhmedSami019 <easami019192@gmail.com>
2025-10-07 12:44:59 +0600

    string charecter count solved

---
+++ count_string.js
@@ -0,0 +1,13 @@
+const text = "A quick brown fox jumps over the lazy dog"
+
+const countCharacter = (text) =>{
+    let count = 0
+    const length = text.length
+
+    for(let i = 0; i<=length ; i++){
+        count = i
+    }
+    console.log(count);
+}
+
+countCharacter(text)
