a25a708515fe98751adeb1934f05d5abb652d1b6
AhmedSami019 <easami019192@gmail.com>
2025-10-07 13:04:51 +0600

    repeat word in multiple time is solve

---
+++ repeat_word.js
@@ -0,0 +1,10 @@
+const repeatWord = (word, times) => {
+    
+    for(let i = 0 ; i <= times; i+=1){
+        console.log(word);
+    }
+}
+
+const word = "Hello"
+const times = 10
+repeatWord(word, times)
